8c115f716549916268441fe78c5a9d2d9d6dff61
Yan Marinho <yan.marinho@pixter.com.br>
2022-11-02 10:50:27 -0300

    🎉 start mobile side project

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text fontSize={25} color="white">
+        SignIn
+      </Text>
+    </Center>
+  );
+}
